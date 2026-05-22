938d5cc490875e8cf0bdddaf4ffe8ee18ef87684
Azaken1248 <suvkrm@gmail.com>
2026-05-22 12:10:52 +0530

    add framer mortion presence animation wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { PortfolioProvider } from './content/usePortfolio.tsx'
+import { PortfolioProvider, usePortfolioFetch } from './content/usePortfolio.tsx'
+import { LoadingSkeleton } from './components/LoadingSkeleton.tsx'
+import { AnimatePresence } from 'framer-motion'
+
+function Root() {
+  const { data, isLoading } = usePortfolioFetch()
+
+  return (
+    <AnimatePresence mode="wait">
+      {isLoading ? (
+        <LoadingSkeleton key="skeleton" />
+      ) : (
+        <PortfolioProvider data={data} key="app">
+          <App />
+        </PortfolioProvider>
+      )}
+    </AnimatePresence>
+  )
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PortfolioProvider>
-      <App />
-    </PortfolioProvider>
+    <Root />
   </StrictMode>,
 )
